7642b1f02cf92baed56c276fb18c15112426a5b7
Andreas Motl <andreas.motl@panodata.org>
2022-07-19 03:06:39 +0200

    UDP receiver based on `asyncio-dgram` and `pynmea2`

---
+++ nmea_kindle_panel/cli.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# (c) 2022 Andreas Motl <andreas.motl@panodata.org>
+# License: GNU Affero General Public License, Version 3
+import logging
+import sys
+
+import click
+
+from nmea_kindle_panel.core import UdpNmeaMessageReceiver
+from nmea_kindle_panel.util import make_sync, setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.version_option()
+@click.option("--verbose", is_flag=True, required=False, help="Increase log verbosity.")
+@click.option("--debug", is_flag=True, required=False, help="Enable debug messages.")
+@click.pass_context
+def cli(ctx, verbose, debug):
+    setup_logging(level=logging.INFO)
+
+
+@click.command()
+@click.option("--source", type=str, required=False, help="Receive telemetry data from source")
+@click.pass_context
+@make_sync
+async def log(ctx, source: str):
+    receiver = UdpNmeaMessageReceiver()
+    async for message in receiver.read():
+        logger.info(f"Decoded message: {dict(message)}")
+    print("Ready.", file=sys.stderr)
+
+
+cli.add_command(log, name="log")
---
+++ nmea_kindle_panel/util.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# (c) 2022 Andreas Motl <andreas.motl@panodata.org>
+# License: GNU Affero General Public License, Version 3
+import asyncio
+import dataclasses
+import enum
+import functools
+import json
+import logging
+import sys
+import typing as t
+from collections import OrderedDict
+
+import click
+from pynmea2 import NMEASentence
+
+
+def setup_logging(level=logging.INFO):
+    log_format = "%(asctime)-15s [%(name)-25s] %(levelname)-8s: %(message)s"
+    logging.basicConfig(format=log_format, stream=sys.stderr, level=level)
+
+
+def make_sync(func):
+    """
+    Click entrypoint decorator for wrapping asynchronous functions.
+
+    https://github.com/pallets/click/issues/2033
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(func(*args, **kwargs))
+
+    return wrapper
+
+
+class JsonEncoderPlus(json.JSONEncoder):
+    """
+    JSON encoder with support for serializing Enums and Data Classes.
+
+    - https://docs.python.org/3/library/json.html#json.JSONEncoder
+    - https://docs.python.org/3/library/enum.html
+    - https://docs.python.org/3/library/dataclasses.html
+    """
+
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+def to_json(obj, pretty=True):
+    """
+    Serialize any object to JSON by using a custom encoder.
+    """
+    kwargs = {}
+    if pretty:
+        kwargs["indent"] = 2
+    return json.dumps(obj, cls=JsonEncoderPlus, **kwargs)
+
+
+class EnumChoice(click.Choice):
+    # https://github.com/pallets/click/pull/2210
+    def __init__(self, enum_type: t.Type[enum.Enum], case_sensitive: bool = True):
+        super().__init__(
+            choices=[element.name for element in enum_type],
+            case_sensitive=case_sensitive,
+        )
+        self.enum_type = enum_type
+
+    def convert(self, value: t.Any, param: t.Optional["Parameter"], ctx: t.Optional["Context"]) -> t.Any:
+        value = super().convert(value=value, param=param, ctx=ctx)
+        if value is None:
+            return None
+        return self.enum_type[value]
+
+
+async def wait_forever():
+    future = asyncio.Future()
+    return await asyncio.wait_for(future, timeout=None)
+
+
+def nmea_asdict(sentence: NMEASentence) -> t.Dict:
+    data = OrderedDict()
+    for field in sentence.fields:
+        name = field[1]
+        value = getattr(sentence, name)
+        data[name] = value
+    return data
